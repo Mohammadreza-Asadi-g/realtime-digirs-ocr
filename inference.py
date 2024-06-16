39289926071a61f14d4f6a6b79fa82af15025d4c
Mohammadreza Asadi G. <mohammadreza.asadi1992@gmail.com>
2024-06-16 19:30:10 +0330

    Update inference.py
    opencv camera initialization

---
+++ inference.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+# Using Phone Camera
+cap  = cv.VideoCapture('http://192.168.0.35:4747/video') 
+
+if not cap.isOpened():
+    print("Cannot open camera!")
+    exit()
+	
+while(True): 
+
+	ret, frame = cap.read()
+	if ret:
+		cv.imshow('frame', frame) 
+	
+	if cv.waitKey(1) & 0xFF == ord('q'): 
+		break
+
+cap.release() 
+cv.destroyAllWindows() 
